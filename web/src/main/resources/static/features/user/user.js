c3cd90cb6fa47160545af980eadd467d019c44de
Souchet Céline <souchet.celine.5@gmail.com>
2015-11-02 23:40:43 +0100

    Remove unused directory

--- web/src/main/resources/static/features/user/user.js
+++
@@ -1,6 +0,0 @@
-/**
- * Copyright 2015 Souchet Céline
- */
-
-angular.module('org.csouchet.gbsmith.features.user', ['org.csouchet.gbsmith.features.user.controller',
-    'org.csouchet.gbsmith.features.user.service']);
